8cd7d613c6f55e1b6a476e14a35067006f1640ae
LukasElenbergas <lukas.elenbergas@gmail.com>
2023-09-07 09:24:00 +0300

    Team project related updates

--- Helpers/download_musiccaps_modified.py
+++ Helpers/download_musiccaps_modified.py
@@ -13,6 +13,15 @@ from pathlib import Path
 
 from datasets import load_dataset, Audio
 
+MUSIC_TO_SKIP = ['-sevczF5etI', '0J_2K1Gvruk', '0fqtA_ZBn_8', '0khKvVDyYV4', '0pewITE1550', '25Ccp8usBtE', '2dyxjGTXSpA', '374R7te0ra0', '5Y_mT93tkvQ', '63rqIYPHvlc', 
+                 '6xxu6f0f0e4', '7B1OAtD_VIA', '7WZwlOrRELI', '8olHAhUKkuk', 'Ah_aYOGnQ_I', 'B7iRvj8y9aU', 'BeFzozm_H5M', 'BiQik0xsWxk', 'C7OIuhWSbjU', 'CCFYOw8keiI', 
+                 'EhFWLbNBOxc', 'Fv9swdLA-lo', 'HAHn_zB47ig', 'Hvs6Xwc6-gc', 'IbJh1xeBFcI', 'JNw0A8pRnsQ', 'Jk2mvFrdZTU', 'L5Uu_0xEZg4', 'LRfVQsnaVQE', 'MYtq46rNsCA', 
+                 'NIcsJ8sEd0M', 'NXuB3ZEpM5U', 'OS4YFp3DiEE', 'RQ0-sjpAPKU', 'Rqv5fu1PCXA', 'SLq-Co_szYo', 'T6iv9GFIVyU', 'TkclVqlyKx4', 'UdA6I_tXVHE', 'Vu7ZUUl4VPc', 
+                 'We0WIPYrtRE', 'WvEtOYCShfM', 'Xoke1wUwEXY', 'Xy7KtmHMQzU', 'ZZrvO__SNtA', '_ACyiKGpD8Y', '_DHMdtRRJzE', '_hYBs0xee9Y', 'asYb6iDz_kM', 'cADT8fUucLQ', 
+                 'd6-bQMCz7j0', 'dcY062mkf9g', 'eHeUipPZHIc', 'ed-1zAOr9PQ', 'fZyq2pM2-dI', 'fwXh_lMOqu0', 'g8USMvt9np0', 'gdtw54I8soM', 'go_7i6WvfeE', 'iXgEQj1Fs7g', 
+                 'idVCpQaByc4', 'j9hAUlz5kQs', 'jd1IS7N3u0I', 'jmPmqzxlOTY', 'k-LkhT4HAiE', 'kiu-40_T5nY', 'lTLsL94ABRs', 'lrk00BNiuD4', 'm-e3w2yZ6sM', 'nTtxF9Wyw6o',
+                 'p_-lKpxLK3g', 'qc1DaM4kdO0', 't5fW1-6iXZY', 'tpamd6BKYU4', 'vOAXAoHtl7o', 'vQHKa69Mkzo', 'vVNWjq9byoQ', 'xxCnmao8FAs', 'zCrpaLEq1VQ', 'zSSIGv82318', 
+                 'zSq2D_GF00o']
 
 def download_clip(
     video_identifier,
@@ -65,6 +74,8 @@ def main(
     """
 
     ds = load_dataset('google/MusicCaps', split='train')
+    ds = ds.filter(lambda example: example['ytid'] not in MUSIC_TO_SKIP)
+
     if limit is not None:
         print(f"Limiting to {limit} examples")
         ds = ds.select(range(limit))
